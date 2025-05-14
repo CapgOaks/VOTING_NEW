baddc7df1372ce2b1e981fd6d9d6db917ce3c00e
Lakshit Bagul <lakshitbagul777@gmail.com>
2025-05-14 20:32:14 +0530

    Added Results service, repo

--- src/main/java/com/capgemini/security4/exception/GlobalExceptionHandler.java
+++ src/main/java/com/capgemini/security4/exception/GlobalExceptionHandler.java
@@ -12,10 +12,14 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
 	}
-	
 
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<String> handleUserAlreadyExistsException(UserNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+
+	@ExceptionHandler(ResultNotFoundException.class)
+	public ResponseEntity<String> handleResultNotFoundException(ResultNotFoundException ex) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
 	}
 }
---
+++ src/main/java/com/capgemini/security4/exception/ResultNotFoundException.java
@@ -0,0 +1,7 @@
+package com.capgemini.security4.exception;
+
+public class ResultNotFoundException extends RuntimeException {
+	public ResultNotFoundException(String message) {
+		super(message);
+	}
+}
