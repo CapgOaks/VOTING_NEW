64edfd01444c807feb0db210ca36fd8642e81032
Om Gade <omgade189@gmail.com>
2025-05-15 14:45:28 +0530

    commit

--- src/main/java/com/capgemini/security4/security/SecurityConfig.java
+++ src/main/java/com/capgemini/security4/security/SecurityConfig.java
@@ -57,11 +57,14 @@ public class SecurityConfig {
 		return http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults())
 				.exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint))
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth.requestMatchers("/auth/signin", "/auth/register","/web-pages/**").permitAll()
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/auth/signin", "/auth/register","/web-pages/**")
+						.permitAll()
 						.requestMatchers("/api/**").hasAnyRole("USER", "ADMIN").anyRequest().authenticated())
 				.authenticationProvider(authenticationProvider())
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
 	}
+	
+	
 
 }
 
